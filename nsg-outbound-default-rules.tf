0aacb5e9fb612b508763b4e44d3b1411b071d984
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2019-11-22 19:43:16 -0300

    add new files

--- nsg-outbound-default-rules.tf
+++ nsg-outbound-default-rules.tf
@@ -11,3 +11,17 @@ resource "azurerm_network_security_rule" "network_security_rule_custom_deny_all_
   resource_group_name         = "FinancialProducts-Common-CE-STG"
   network_security_group_name = azurerm_network_security_group.network_security_group.name
 }
+
+resource "azurerm_network_security_rule" "network_security_rule_custom_allow_internet_outbound" {
+  name                        = "CustomAllowInternetOutBound"
+  priority                    = 3000
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "VirtualNetwork"
+  destination_address_prefix  = "Internet"
+  resource_group_name         = "FinancialProducts-Common-CE-STG"
+  network_security_group_name = azurerm_network_security_group.network_security_group.name
+}
