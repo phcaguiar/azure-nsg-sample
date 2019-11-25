2687c4df1160a24066f07dae8533ec4ee9f65142
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2019-11-25 16:39:48 -0300

    add inbound rules from private dc to vnet

--- nsg-inbound-baseline-rules.tf
+++ nsg-inbound-baseline-rules.tf
@@ -39,3 +39,17 @@ resource "azurerm_network_security_rule" "network_security_rule_custom_allow_all
   resource_group_name         = var.network_resource_group_name
   network_security_group_name = azurerm_network_security_group.baseline_network_security_group.name
 }
+
+resource "azurerm_network_security_rule" "network_security_rule_custom_allow_private_datacenters_inbound" {
+  name                        = "Custom_Allow_Private_Datacenters_InBound"
+  priority                    = 4094
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefixes     = ["var.private_datacenters_networks"]
+  destination_address_prefix  = "VirtualNetwork"
+  resource_group_name         = var.network_resource_group_name
+  network_security_group_name = azurerm_network_security_group.baseline_network_security_group.name
+}
